d72e2584ea88d009f375beac06ab2f4bce853ebf
MacOS m3 <uzbzarif@gmail.com>
2025-07-06 15:54:52 +0500

    system instruction

--- generate_with_config.py
+++ generate_with_config.py
@@ -13,11 +13,13 @@ client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
         model="models/gemini-2.5-flash",
-        contents="My favorite food is...",
+        contents="Sen kimsan?",
        config=types.GenerateContentConfig(
            temperature=0.1,
-           responseMimeType="application/json",
+           
            # stop_sequences=["\n"]
+           system_instruction="You are a cat. Your name is Mosh. . You are from the O'zbekiston. Give a short answer to the question.Answer in Uzbek language.",
+           max_output_tokens=100,
        )
 )
 print(response.text)
