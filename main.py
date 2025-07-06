5bed0d89b597767f7dd157460fedbd95a2d79b0a
MacOS m3 <uzbzarif@gmail.com>
2025-07-06 16:30:04 +0500

    .

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
-
 from dotenv import load_dotenv
 import os
 from google import genai
 
-# Load environment variables from .env file
 load_dotenv()
-# Set up the API key
-API_KEY = os.getenv("GOOGLE_API_KEY")
-client = genai.Client(api_key=API_KEY)
+# Get the API key from the environment variable
+api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+
+response = client.models.generate_content(
+        model="gemma-3n-e2b-it", 
+        contents="The cat jumped over the ...",
+)
+print(response.text)
 
 # Get all available models
-models = client.models.list()
+# models = client.models.list()
 
-# Print model names
-for model in models:
-    print(type(model))
-    print(f"""
-    Model Name: {model.name}
-    Model Base: {model.display_name}
-    Model Description: {model.description}
-    Model Version: {model.version}
-    """)
+# # Print model names
+# for model in models:
+#     print(type(model))
+#     print(f"""
+#     Model Name: {model.name}
+#     Model Base: {model.display_name}
+#     Model Description: {model.description}
+#     Model Version: {model.version}
+#     """)
           
 
-# response = client.models.generate_content(
-#     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
-# )
-# print(response.text)
+# # response = client.models.generate_content(
+# #     model="gemini-2.0-flash", contents="Explain how AI works in a few words"
+# # )
+# # print(response.text)
